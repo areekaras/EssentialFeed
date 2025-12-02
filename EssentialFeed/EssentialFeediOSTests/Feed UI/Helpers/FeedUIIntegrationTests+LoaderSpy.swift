989cc005058cb88b2409ef7ff67b80cb1f169a5e
areekaras <shibili.areekara@gmail.com>
2025-12-03 06:05:55 +0800

    Rename `FeedUIViewControllerTests` to `FeedUIIntegrationTests` since we  are testing the composition of multiple UI components in integration

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
@@ -1,5 +1,5 @@
 //
-//  FeedViewControllerTests+LoaderSpy.swift
+//  FeedUIIntegrationTests+LoaderSpy.swift
 //  EssentialFeediOSTests
 //
 //  Created by Shibili Areekara on 01/11/25.
@@ -9,7 +9,7 @@ import Foundation
 import EssentialFeed
 import EssentialFeediOS
 
-extension FeedViewControllerTests {
+extension FeedUIIntegrationTests {
     class LoaderSpy: FeedLoader, FeedImageDataLoader {
         
         // MARK: - FeedLoader
