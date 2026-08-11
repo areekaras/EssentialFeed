f9258b0f038fa419c368fa51251200ad81da75ef
areekaras <shibili.areekara@gmail.com>
2026-08-11 10:01:16 +0530

    Extract test helpers into a shared scope to remove duplication

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -67,12 +67,8 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
             id: UUID(),
             description: "any",
             location: "any",
-            url: URL(string: "http://any-url.com")!
+            url: anyURL()
         )]
-    }
-    
-    private func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 0)
     }
     
     private class LoaderStub: FeedLoader {
