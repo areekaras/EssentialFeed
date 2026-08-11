601fb5d544b816f28a07cca0642db7dd987c558e
areekaras <shibili.areekara@gmail.com>
2026-08-11 15:18:50 +0530

    Move `FeedLoaderCacheDecorator` to production

---
+++ EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
@@ -0,0 +1,27 @@
+//
+//  FeedLoaderCacheDecorator.swift
+//  EssentialApp
+//
+//  Created by Shibili Areekara on 11/08/26.
+//
+
+import EssentialFeed
+
+public class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    private let cache: FeedCache
+    
+    public init(decoratee: FeedLoader, cache: FeedCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.cache.save(feed) { _ in }
+            }
+            completion(result)
+        }
+    }
+}
